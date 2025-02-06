cbb51d2c110c8a68d3a2877bfd77ae3dcd6b1953
Christiaan Landman <chriz.ek@gmail.com>
2025-02-06 15:56:10 +0200

    Scaffoling AbstractRemote/StreamingSyncTypes.

---
+++ src/Common/Client/Sync/Stream/StreamingSyncTypes.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using Common.Client.Sync.Bucket;
+using Common.DB.Crud;
+using Newtonsoft.Json;
+
+public class ContinueCheckpointRequest
+{
+    [JsonProperty("buckets")]
+    public List<BucketRequest> Buckets { get; set; } = new();
+
+    [JsonProperty("checkpoint_token")]
+    public string CheckpointToken { get; set; } = "";
+
+    [JsonProperty("limit")]
+    public int? Limit { get; set; }
+}
+
+public class SyncNewCheckpointRequest
+{
+    [JsonProperty("buckets")]
+    public List<BucketRequest>? Buckets { get; set; }
+
+    [JsonProperty("request_checkpoint")]
+    public RequestCheckpoint RequestCheckpoint { get; set; } = new();
+
+    [JsonProperty("limit")]
+    public int? Limit { get; set; }
+}
+
+public class RequestCheckpoint
+{
+    [JsonProperty("include_data")]
+    public bool IncludeData { get; set; }
+
+    [JsonProperty("include_checksum")]
+    public bool IncludeChecksum { get; set; }
+}
+
+public class SyncResponse
+{
+    [JsonProperty("data")]
+    public List<SyncDataBucketJSON>? Data { get; set; }
+
+    [JsonProperty("has_more")]
+    public bool HasMore { get; set; }
+
+    [JsonProperty("checkpoint_token")]
+    public string? CheckpointToken { get; set; }
+
+    [JsonProperty("checkpoint")]
+    public Checkpoint? Checkpoint { get; set; }
+}
+
+public class StreamingSyncRequest
+{
+    [JsonProperty("buckets")]
+    public List<BucketRequest>? Buckets { get; set; }
+
+    [JsonProperty("only")]
+    public List<string>? Only { get; set; }
+
+    [JsonProperty("include_checksum")]
+    public bool IncludeChecksum { get; set; }
+
+    [JsonProperty("raw_data")]
+    public bool RawData { get; set; }
+
+    [JsonProperty("parameters")]
+    public Dictionary<string, object>? Parameters { get; set; }
+
+    [JsonProperty("client_id")]
+    public string? ClientId { get; set; }
+}
+
+public class BucketRequest
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = "";
+
+    [JsonProperty("after")]
+    public string After { get; set; } = "";
+}
+
+public class StreamingSyncCheckpoint
+{
+    [JsonProperty("checkpoint")]
+    public Checkpoint Checkpoint { get; set; } = new();
+}
+
+public class StreamingSyncCheckpointDiff
+{
+    [JsonProperty("checkpoint_diff")]
+    public CheckpointDiff CheckpointDiff { get; set; } = new();
+}
+
+public class CheckpointDiff
+{
+    [JsonProperty("last_op_id")]
+    public string LastOpId { get; set; } = "";
+
+    [JsonProperty("updated_buckets")]
+    public List<BucketChecksum> UpdatedBuckets { get; set; } = new();
+
+    [JsonProperty("removed_buckets")]
+    public List<string> RemovedBuckets { get; set; } = new();
+
+    [JsonProperty("write_checkpoint")]
+    public string WriteCheckpoint { get; set; } = "";
+}
+
+public class StreamingSyncDataJSON
+{
+    [JsonProperty("data")]
+    public SyncDataBucketJSON Data { get; set; } = new();
+}
+
+public class StreamingSyncCheckpointComplete
+{
+    [JsonProperty("checkpoint_complete")]
+    public CheckpointComplete CheckpointComplete { get; set; } = new();
+}
+
+public class CheckpointComplete
+{
+    [JsonProperty("last_op_id")]
+    public string LastOpId { get; set; } = "";
+}
+
+public class StreamingSyncKeepalive
+{
+    [JsonProperty("token_expires_in")]
+    public int TokenExpiresIn { get; set; }
+}
+
+public class CrudRequest
+{
+    [JsonProperty("data")]
+    public List<CrudEntry> Data { get; set; } = new();
+}
+
+public class CrudResponse
+{
+    [JsonProperty("checkpoint")]
+    public string? Checkpoint { get; set; }
+}
