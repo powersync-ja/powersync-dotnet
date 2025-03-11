cfbfe071eb4918561f9c7fd44fdde88b689d4e37
Christiaan Landman <chriz.ek@gmail.com>
2025-03-11 08:48:52 +0200

    Added setup for MAUI app, before PS is working on it.

---
+++ demos/TodoSQLite/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
+using TodoSQLite.Data;
+using TodoSQLite.Views;
+
+namespace TodoSQLite;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+		builder.Logging.SetMinimumLevel(LogLevel.Debug);
+		builder.Logging.AddDebug();
+		builder.Services.AddSingleton<TodoListPage>();
+		builder.Services.AddTransient<TodoItemPage>();
+
+		builder.Services.AddSingleton<TodoItemDatabase>();
+
+		return builder.Build();
+	}
+}
