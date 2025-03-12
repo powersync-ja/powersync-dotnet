7f795abc571a165c4a016828091c49e28953cba5
Christiaan Landman <chriz.ek@gmail.com>
2025-03-12 15:46:33 +0200

    Removed `src` contents that were missed in renaming of directories.

--- src/Common/Client/Connection/PowerSyncCredentials.cs
+++
@@ -1,7 +0,0 @@
-namespace Common.Client.Connection;
-public class PowerSyncCredentials(string endpoint, string token, DateTime? expiresAt = null)
-{
-    public string Endpoint { get; set; } = endpoint;
-    public string Token { get; set; } = token;
-    public DateTime? ExpiresAt { get; set; } = expiresAt;
-}
