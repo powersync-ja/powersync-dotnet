0813194f47d80ad0ae802135152064fabf2e1d17
Christiaan Landman <chriz.ek@gmail.com>
2025-07-01 13:20:00 +0200

    Renamed TodoSqlite demo to MAUITodo.

---
+++ demos/MAUITodo/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MAUITodo.Views;
+
+namespace MAUITodo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+		Routing.RegisterRoute(nameof(TodoListPage), typeof(TodoListPage));
+		Routing.RegisterRoute(nameof(SqlConsolePage), typeof(SqlConsolePage));
+	}
+}
