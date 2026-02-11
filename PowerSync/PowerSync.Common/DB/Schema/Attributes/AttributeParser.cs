d438ff57674bdee0b5c6b9d8e644dc46f696ded6
LucDeCaf <luc@powersync.com>
2026-02-11 15:01:18 +0200

    Changelog

--- PowerSync/PowerSync.Common/DB/Schema/Attributes/AttributeParser.cs
+++ PowerSync/PowerSync.Common/DB/Schema/Attributes/AttributeParser.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 using Dapper;
 
-internal class AttributeParser
+class AttributeParser
 {
     private readonly Type _type;
     private readonly TableAttribute _tableAttr;
