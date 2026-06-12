faff4214bb7aacbf6edcd7ce11cac71ad538adfa
LucDeCaf <luc@powersync.com>
2026-06-12 16:36:15 +0200

    Update comment

--- PowerSync/PowerSync.Common/DB/Schema/ColumnType.cs
+++ PowerSync/PowerSync.Common/DB/Schema/ColumnType.cs
@@ -6,8 +6,8 @@ public enum ColumnType
     Integer,
     Real,
     /// <summary>
-    /// <para>Infers the column type based on the associated property's PropertyType.</para>
-    /// <para>**NB:** `ColumnType.Inferred` can only be used when using the schema attributes syntax.</para>
+    /// Infers the column type based on the associated property's PropertyType.
+    /// <b>NB:</b> `ColumnType.Inferred` can only be used when using the <see cref="Attributes" /> syntax.
     /// </summary>
     Inferred
 }
