c418998c3307a928259c39dff8eb5c0cc1302622
LucDeCaf <luc@powersync.com>
2026-09-14 15:01:47 +0200

    Fix Tools/Setup

--- Tools/Setup/Setup.cs
+++ Tools/Setup/Setup.cs
@@ -1,3 +1,9 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.IO.Compression;
 using System.Xml;
 using System.Xml.Linq;
